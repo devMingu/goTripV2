5a01fb62d7f10f4bd4e18d398ad2a225d0954216
devMingu <rooster100@naver.com>
2022-07-13 13:20:02 +0900

    [feat] movie style change

--- public/JS/search.js
+++ public/JS/search.js
@@ -4,30 +4,37 @@ let backgroundFlag = 0;
 let tour = [
     {
         location: '유럽',
+        name: 'europe',
         img: 'https://source.unsplash.com/collection/2141901/122x122'
     },
     {
         location: '아시아',
+        name: 'asia',
         img: 'https://source.unsplash.com/collection/2084453/122x122'
     },
     {
         location: '지중해',
+        name: 'mediterraneanSea',
         img: 'https://source.unsplash.com/collection/10526814/122x122'
     },
     {
         location: '한국',
+        name: 'korea',
         img: 'https://source.unsplash.com/collection/10750452/122x122'
     },
     {
         location: '일본',
+        name: 'japan',
         img: 'https://source.unsplash.com/collection/8515895/122x122'
     },
     {
         location: '이탈리아',
+        name: 'italia',
         img: 'https://source.unsplash.com/collection/45135470/122x122'
     },
     {
         location: '스칸디나비아',
+        name: 'scandinavia',
         img: 'https://source.unsplash.com/collection/1784623/122x122'
     },
 ];
@@ -39,10 +46,13 @@ for(let i =0; i<tour.length; i++){
     const divTag = document.createElement('div');
     const divLocationTag = document.createElement('div');
     const imgTag = document.createElement('img');
-    
+    const aTag = document.createElement('a');
+    aTag.href = `/goTrip/${tour[i].name}`;
     imgTag.src = tour[i].img;
     divLocationTag.textContent = tour[i].location;
-    divTag.appendChild(imgTag);
+    
+    aTag.appendChild(imgTag);
+    divTag.appendChild(aTag);
     divTag.appendChild(divLocationTag);
     divTag.classList.add('tourListBox');
     tourList.appendChild(divTag);
