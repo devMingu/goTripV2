d0a8dedc68f0f5ff7b77a80a01dc3ec6b6f361c5
devMingu <rooster100@naver.com>
2022-07-25 19:32:05 +0900

    [feat] mongo1

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const port = process.env.PORT || 3030;
 const mongoose = require('mongoose');
 const Userdata = require('./model/userModel/userData');
 // mongoose.connect('mongodb://localhost:27017/goTrip');
-mongoose.connect("mongodb+srv://wintermingu12:!Mrlaalsrn12@dbgotripuser.enhtf48.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://wintermingu12:!Mrlaalsrn12@dbgotripuser.enhtf48.mongodb.net/?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 const Userdata = require('../model/userModel/userData');
 // mongoose.connect('mongodb://localhost:27017/goTrip');
-mongoose.connect("mongodb+srv://wintermingu12:!Mrlaalsrn12@dbgotripuser.enhtf48.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://wintermingu12:!Mrlaalsrn12@dbgotripuser.enhtf48.mongodb.net/?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
