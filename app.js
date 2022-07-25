0a40fe28cd19bf7666e1cf3b4a3bf430f711a193
devMingu <rooster100@naver.com>
2022-07-25 19:41:18 +0900

    [feat] mongo4

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const Userdata = require('./model/userModel/userData');
 //     useCreateIndex: true,
 //     useUnifiedTopology: true
 // });
-goose.connect(process.env.MONGO_DB);
+mongoose.connect(process.env.MONGO_DB);
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
