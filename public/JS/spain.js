de7ce02e2a4885c5356a54b55c68e3a65758103e
devMingu <rooster100@naver.com>
2022-08-23 12:58:31 +0900

    [feat] animation

--- public/JS/spain.js
+++ public/JS/spain.js
@@ -1,9 +1,19 @@
-const spainCount = document.querySelector(".spainCount");
+const onTitle = document.querySelector(".onTitle");
+const tripContentOne = document.querySelector(".tripContentOne");
+const spainCampingIcon = document.querySelector(".spainCampingIcon");
+const tripContentOneTitle = document.querySelector(".tripContentOneTitle");
+const tripContentOneTitles = document.querySelector('.tripContentOneTitles');
+const tripContentSubTitle = document.querySelector(".tripContentSubTitle");
 
-let cnt = 0;
-spainCount.textContent = cnt;
+const iconClick = () => {
+    console.log("ICON CLICKED");
+    tripContentOne.classList.toggle('testUrl');
+    spainCampingIcon.classList.toggle('hide');
+    tripContentOneTitle.textContent = "열정 가득한 나라";
+    tripContentOneTitles.textContent = "뜨거운 열정 속으로"
+    tripContentOneTitles.classList.add('tripTitleContentCntName');
+    tripContentSubTitle.textContent = "인천에서 마드리드 대한항공 직항으로 이용해보세요"
+};
 
-spainCount.addEventListener('click', ()=>{
-    cnt++;
-    spainCount.textContent = cnt;
-})
+onTitle.addEventListener('click', iconClick);
+
