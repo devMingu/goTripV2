4205211fd2b5cfbfc449c66ac0cb5c798f67060c
devMingu <rooster100@naver.com>
2023-01-11 21:20:15 +0900

    [fix]

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const Userdata = require('./model/userModel/userData');
-const mongoURL = process.env.MONGODB_URL || "mongodb+srv://wintermingu12:!Mrlaalsrn12@dbgotripuser.enhtf48.mongodb.net/test";
-// mongoose.connect("mongodb+srv://wintermingu12:!Mrlaalsrn12@dbgotripuser.enhtf48.mongodb.net/test");
+const mongoURL = process.env.MONGODB_URL;
 mongoose.connect(mongoURL);
 const db = mongoose.connection;
 
