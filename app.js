30d359ccbf3ab6459a90f747602a9366c5ef98cc
devMingu <rooster100@naver.com>
2023-01-11 21:11:16 +0900

    [fix] 몽고DB 수정

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const Userdata = require('./model/userModel/userData');
-mongoose.connect("mongodb+srv://wintermingu12:!Mrlaalsrn12@dbgotripuser.enhtf48.mongodb.net/test");
-
+const mongoURL = process.env.MONGODB_URL || "mongodb+srv://wintermingu12:!Mrlaalsrn12@dbgotripuser.enhtf48.mongodb.net/test";
+// mongoose.connect("mongodb+srv://wintermingu12:!Mrlaalsrn12@dbgotripuser.enhtf48.mongodb.net/test");
+mongoose.connect(mongoURL);
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
